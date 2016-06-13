8804594b5e5f370cdaae01d3b2e25b4c37c52612
MishaHerscu <mishaherscu@gmail.com>
2016-06-13 10:34:12 -0400

    finished lab

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,1 +1,20 @@
 require_relative 'shape'
+
+# Rectangle class
+class Rectangle < Shape
+  def initialize(length, width)
+    @length = length
+    @width = width
+    @num_sides = 4
+    super(@num_sides, @length)
+  end
+
+  def area
+    @length * @width
+  end
+end
+
+# tests
+my_rectangle = Rectangle.new(4, 3)
+p my_rectangle
+p "Area: #{my_rectangle.area}"
